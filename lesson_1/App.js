696746f4b7d897cd38eb2df5ca1da6b3cd3d5a6d
Sakitsudzukeru <101800445+Sakitsudzukeru@users.noreply.github.com>
2022-05-02 16:01:09 +0300

    last commit

--- lesson_1/App.js
+++ lesson_1/App.js
@@ -1,28 +1,33 @@
 
- function playSound(e) {
-  // Переменная const audio определяет HTML-элементы, которые воспроизводят звуки барабана 
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    // Переменная const key определяет HTML-элементы по их кодам клавиш, 
-    // чтобы при нажатии определенной клавиши воспроизводился правильный звук
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-    // На простом английском языке в нем говорится: “если срабатывающий тег <audio>
-    //  не имеет атрибута data-key keyCode, выйдите из функции”. 
-    if (!audio) return
-  
-    // Чтобы позволить клавише воспроизводить звук повторно,  нужно перемотать (сбросить) звук. 
-    // для этого нужно audio.currentTime сделать равным 0. 
-    audio.currentTime = 0
-    audio.play()  
-    key.classList.add('playing')
-  }
 
-  function removeTransition(e) {
-    if (e.propertyName !== 'transform') return
-    e.target.classList.remove('playing')
+function playSound(e) {
+  // Переменная const audio определяет HTML-элементы, которые воспроизводят звуки барабана 
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  // Переменная const key определяет HTML-элементы по их кодам клавиш, 
+  // чтобы при нажатии определенной клавиши воспроизводился правильный звук
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  // На простом английском языке в нем говорится: “если срабатывающий тег <audio>
+  //  не имеет атрибута data-key keyCode, выйдите из функции”. 
+  if (!audio) return
+
+  // Чтобы позволить клавише воспроизводить звук повторно,  нужно перемотать (сбросить) звук. 
+  // для этого нужно audio.currentTime сделать равным 0. 
+  audio.currentTime = 0
+  audio.play()
+  key.classList.add('playing')
+}
+
+function removeTransition(e) {
+  if (e.target.classList.contains('playing')) {
+    e.target.classList.remove('playing');
   }
-  const keys = Array.from(document.querySelectorAll('.key'))
-  // цикл forEach(), для того чтобы прослушиватель событий был на каждой клавише клавиатуры
-  keys.forEach((key) => key.addEventListener('transitionend', removeTransition))
-  // Чтобы воспроизвести правильный звук при нажатии клавиши, нужен прослушиватель событий, 
-  // который будет прослушивать событие нажатия клавиши с клавиатуры - это keydown 
-  window.addEventListener('keydown', playSound)
+  else {
+    return;
+  }
+}
+const keys = Array.from(document.querySelectorAll('.key'))
+// цикл forEach(), для того чтобы прослушиватель событий был на каждой клавише клавиатуры
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition))
+// Чтобы воспроизвести правильный звук при нажатии клавиши, нужен прослушиватель событий, 
+// который будет прослушивать событие нажатия клавиши с клавиатуры - это keydown 
+window.addEventListener('keydown', playSound)
