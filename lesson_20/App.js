8cb08625f10acb7dfba901222e3be66f7ef411cd
Sakitsudzukeru <101800445+Sakitsudzukeru@users.noreply.github.com>
2022-05-17 09:58:59 +0300

    + lesson 19-21

---
+++ lesson_20/App.js
@@ -0,0 +1,25 @@
+window.recognition=new SpeechRecognition=window.SpeechRecognition||window.webkitSpeechRecognition
+const recognition =new SpeechRecognition()
+recognition.interimResult=true
+
+let p=document.createElement('p')
+const words=document.querySelector('.words')
+words.appendChild(p)
+
+recognition.addEventListener('results', e=>{
+
+const transcription=Array.from(e.results)
+.map(result=>result[0])
+.map(result=>result.transcription)
+.join('')
+
+p.textContent=transcription
+if(e.results[o].isFinal) {
+    p=document.createElement('p')
+    words.appendChild(p)
+}
+
+})
+
+recognition.addEventListener('end', recognition.start)
+recognition.start()
